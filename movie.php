35a27403bdd536847969f9a6e551eab271e494de
nakarinITI <6306021422089@fitm.kmutnb.ac.th>
2021-03-16 11:54:05 +0700

    s

--- movie.php
+++ movie.php
@@ -3,6 +3,21 @@
     <meta charset="utf-8">
 </head>
 <body>
-    
+    <?php
+        $jsonFile = file_get_contents("movies.json");
+        $array = json_decode($jsonFile,true);
+    ?>
+    <select id="year_movie">
+        <?php 
+        foreach ($array as $key => $value) {
+            echo "<option value='{$value}'>".$value["year"]." : ".$value["title"]."</option>";
+        }
+        ?>
+    </select>
+    <input type="text" name="year" value="">
+    <script>
+        var str="";
+        var jsonEx = <?php echo $jsonFile ?>
+    </script>
 </body>
 </html>
