c1f2de0afb23b69e06b89f3e2dda88e94d19ee9e
nakarinITI <6306021422089@fitm.kmutnb.ac.th>
2021-03-18 14:28:59 +0700

    k

--- test/02rest.php
+++ test/02rest.php
@@ -2,9 +2,10 @@
     include_once "01db.php";
     include_once "util.php";
     $debug_mode = false;
+
     if($_SERVER['REQUEST_METHOD']=='GET'){
         debug_text("GET METHOD Process......",$debug_mode);
-        echo json_encode(show_data());
+        echo json_encode(show_data($debug_mode));
     }else if($_SERVER['REQUEST_METHOD']=='POST'){
         debug_text("POST METHOD May be implement soon...",$debug_mode);
     }else{
@@ -12,10 +13,9 @@
         http_response_code(405);
     }
 
-    function show_data(){
-        $mydb = new db("root","","person_data",true);
+    function show_data($debug_mode){
+        $mydb = new db("root","","person_data",$debug_mode);
         $data = $mydb->query("select * from person");
-        print_r($data);
         $mydb->close();
         return $data;
     }
