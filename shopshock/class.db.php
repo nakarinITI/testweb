5cf4d66c08e670ddcc03dc622214ca7eec0d33bc
nakarinITI <6306021422089@fitm.kmutnb.ac.th>
2021-03-30 10:17:09 +0700

    a

--- shopshock/class.db.php
+++ shopshock/class.db.php
@@ -1,32 +1,3 @@
-<!-- 
-.
-        public function show_product(){
-            $SQL_Query="SELECT p.Product_id, p.Product_code, p.Product_Name, brand.Brand_name, unit.Unit_name, p.Cost FROM product AS p
-            INNER JOIN brand ON (p.Brand_ID = brand.Brand_id) 
-            INNER JOIN unit  ON (p.Unit_ID = unit.Unit_id)";
-            $result = $this->dbConn->query($SQL_Query);
-            $counter=0;
-            while($row = $result->fetch_assoc()){
-                echo"<tr>";
-                foreach($row as $key => $value){
-                    echo "<td>{$value}</td>";
-                }
-                echo "<td><a href='atc.php?id={$row['Product_id']}'>ShopShock</a></td>";
-                echo"</tr>";
-                }
-        }
-
-        public function sel_one($id){
-            $SQL_Query="SELECT p.Product_id, p.Product_code, p.Product_Name, brand.Brand_name, unit.Unit_name, p.Cost FROM product AS p
-            INNER JOIN brand ON (p.Brand_ID = brand.Brand_id) 
-            INNER JOIN unit  ON (p.Unit_ID = unit.Unit_id)
-            WHERE Product_id = '$id'";
-            $result = $this->dbConn->query($SQL_Query);
-            $row = $result->fetch_assoc();
-            return($row);
-        }
-    }
-?> -->
 <?php
     class database{
         private $db;
